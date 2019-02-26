637a8faff3bb75f30e838d8df4f43b707dfa8a40
Tamás Pánczél <tamas.panczel@gmail.com>
2019-02-26 08:29:34 +0100

    some minor fixes

--- README.md
+++ README.md
@@ -26,6 +26,5 @@ select parseSerialized('a:2:{s:3:"tag";s:5:"value";s:5:"array";a:2:{i:0;a:1:{s:3
 ```
 select multiple tags / array:
 ```
-set max_sp_recursion_depth = 3;
 select parseSerialized('a:2:{s:3:"tag";s:5:"value";s:5:"array";a:2:{i:0;a:1:{s:3:"key";s:6:"value1";}i:1;a:1:{s:3:"key";s:6:"value2";}}}', 'array/key', ';')
 ```
--- parseSerialized.sql
+++ parseSerialized.sql
@@ -9,38 +9,53 @@ CREATE PROCEDURE `parseSerializedProc`(
 NO SQL BEGIN
 	declare _path_delim char(1) default '/';
 	declare _path_key varchar(100);
+	declare _path_next_key varchar(100);
 	declare _input_key varchar(100);
+	declare _key_locate varchar(100);
 	declare _type varchar(100);
 	declare _count int;
 	declare _result text;
 
 	set result = '';
 	set _path_key = SUBSTRING_INDEX(path, _path_delim, 1);
-	set _input_key = CONCAT_WS(":", "s", LENGTH(_path_key), CONCAT('"', _path_key, '";'));
-	set input = SUBSTRING(input, LOCATE(_input_key, input, 1) + LENGTH(_input_key));
-	set _type = SUBSTRING(input, 1, 1);
+	set _input_key = IF(LENGTH(_path_key)>0, CONCAT_WS(':', 's', LENGTH(_path_key), CONCAT('"', _path_key, '";')), '');
+	set _key_locate = LOCATE(_input_key, input, 1);
+	set input = IF(_key_locate>0, SUBSTRING(input, _key_locate + LENGTH(_input_key)), '');
+	set _type = SUBSTRING(input, 1, 2);
 
-	if _type = 's' then
+	if _type = 's:' then
 		set result = SUBSTRING_INDEX(SUBSTRING_INDEX(input, '";', 1), ':"', -1);
 	end if;
 
-	if _type = 'i' then
+	if _type = 'i:' then
 		set result = SUBSTRING_INDEX(SUBSTRING_INDEX(input, ';', 1), ':', -1);
 	end if;
 
-	if _type = 'b' then
+	if _type = 'b:' then
 		set result = SUBSTRING_INDEX(SUBSTRING_INDEX(input, ';', 1), ':', -1);
 	end if;
 
-	if _type = 'a' then
+	if _type = 'N;' then
+		set result = 'NULL';
+	end if;
+
+	if _type = 'a:' then
 		set _count = SUBSTRING_INDEX(SUBSTRING_INDEX(input, ':', 2), ':', -1);
+		set _path_next_key = SUBSTRING(path, LENGTH(_path_key) + LENGTH(_path_delim) + 1);
+		set input = IF(LENGTH(_path_next_key)=0, CONCAT(';', input), input);
+
 		while _count > 0 do
-			call parseSerializedProc(input, SUBSTRING(path, LENGTH(_path_key) + LENGTH(_path_delim) + 1), delimiter, _result);
-			set result = CONCAT(result, delimiter, _result);
+			if LENGTH(_path_next_key) = 0 then
+				set input = SUBSTRING(input, LENGTH(SUBSTRING_INDEX(input, ';', 2)) + 2);
+			end if;
+			call parseSerializedProc(input, _path_next_key, delimiter, _result);
+			if _result is not null then
+				set result = CONCAT(result, delimiter, _result);
+			end if;
 			set _count = _count - 1;
 		end while;
-		set result = SUBSTRING(result, 2);
 	end if;
+	set result = TRIM(BOTH delimiter FROM result);
 END$$$
 
 DROP FUNCTION IF EXISTS `parseSerialized`$$$
@@ -51,6 +66,7 @@ CREATE FUNCTION `parseSerialized`(
 RETURNS text NO SQL
 BEGIN
 	declare _result text;
+	set @@session.max_sp_recursion_depth = 100;
 
 	call parseSerializedProc(input, path, delimiter, _result);
 	return _result;
